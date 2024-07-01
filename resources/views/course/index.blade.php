15053be2c1500d954d8a5d926651759f0f971816
Ashish-Dhakal <ashishdhakal433@gmail.com>
2024-07-01 18:12:12 +0545

    add the basic template in blade template

---
+++ resources/views/course/index.blade.php
@@ -0,0 +1,43 @@
+{{-- extending the main layout page start --}}
+
+@extends('main-layout')
+
+{{-- extending the main layout page ends --}}
+
+
+
+{{-- page header start --}}
+
+@section('title', '')
+
+{{-- page header ends --}}
+
+
+{{-- page style starts --}}
+
+@push('style')
+    <style></style>
+@endpush
+
+{{-- page style ends --}}
+
+
+
+{{-- page content section starts --}}
+
+@section('content')
+
+
+@endsection
+
+{{-- page content section ends --}}
+
+
+
+{{-- page script starts --}}
+
+@push('script')
+    <script></script>
+@endpush
+
+{{-- page script ends --}}
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,22 @@
 <?php
 
+use App\Http\Controllers\CourseController;
+use App\Http\Controllers\FreeCourseController;
+use App\Http\Controllers\LiveCourseController;
+use App\Http\Controllers\QuizController;
+use App\Http\Controllers\SubjectController;
+use App\Http\Controllers\UniversityController;
+use App\Models\University;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::resource('university' , UniversityController::class);
+Route::resource('course' , CourseController::class );
+Route::resource('liveCourse' , LiveCourseController::class);
+Route::resource('quiz' , QuizController::class);
+Route::resource('subject' , SubjectController::class);
+Route::resource('freeCourse' , FreeCourseController::class);
